bb7c7857e6f00a349fe1afe9e4cfe5a94c2b8195
Ratnadeep Debnath <rtnpro@gmail.com>
2019-10-01 13:51:53 +0530

    Remove TILLER reference in secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -457,7 +457,7 @@ EOF
 
     # run helm command with args and opts in correct order
     set +e # ignore errors
-    ${HELM_BIN} ${TILLER_HOST:+--host "$TILLER_HOST" }"$cmd" $subcmd "$@" "${cmdopts[@]}"
+    ${HELM_BIN} "$cmd" $subcmd "$@" "${cmdopts[@]}"
     helm_exit_code=$?
     # cleanup on-the-fly decrypted files
     [[ ${#decfiles[@]} -gt 0 ]] && rm -v "${decfiles[@]}"
